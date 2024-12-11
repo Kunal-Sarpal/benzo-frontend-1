b1b2747b8ab3086d467e6959aa62b7555e477281
user.name : "Kunal-Sarpal <user.email: "sarpalkunal7@gmail.com>
2024-12-11 16:35:07 +0530

    home page-1-minor-minor-update-1

--- src/pages/Shop_pages/ShopPage2.jsx
+++ src/pages/Shop_pages/ShopPage2.jsx
@@ -6,9 +6,9 @@ const ShopPage2 = () => {
     return (
         <div className="w-full border-2 bg-[#FAFAFA] min-h-screen">
             {/* Header Section */}
-            <header className=" text-white text-center py-4">
-                <h1 className="text-4xl font-bold">Our Products..</h1>
-                <p className="mt-2 text-lg">Explore Our Latest Products</p>
+            <header className=" text-white text-center py-4 mt-10">
+                <h1 className="text-4xl font-normal text-green-500">Our Products</h1>
+                <p className="mt-2 text-lg text-black">Explore Our Latest Products</p>
             </header>
 
             {/* Main Content: Product Listing */}
